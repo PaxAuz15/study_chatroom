a018eb3f899830ac6d3f83b0d5ee9ffc4c7a7a25
Luis Auz <luisauz.geek@gmail.com>
2021-10-21 22:11:22 -0500

    feat: add views on base app & management urls and views from custom app & link to django url config

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(responnse):
+    return HttpResponse("Home View")
+
+def room(response):
+    return HttpResponse("ROOM")
