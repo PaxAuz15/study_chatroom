6acfc1881e3cbe6012bc82e01ba675d7395eafdb
Luis Auz <luisauz.geek@gmail.com>
2021-10-25 18:50:17 -0500

    feat: Rooms model on DB

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+    # participants = 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
