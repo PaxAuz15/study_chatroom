5673c0a984423fe25bf2980a3c24013ae549e831
Luis Auz <luisauz.geek@gmail.com>
2021-10-25 18:04:48 -0500

    feat: using data and work with Django Template Engine to use that data!

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+    {'id':1, 'name': 'Lets learn python!'},
+    {'id':2, 'name': 'Design with me!'},
+    {'id':3, 'name': 'Frontends Developers'},
+]
+
 def home(request):
-    return render(request=request,template_name='home.html')
+    return render(request,'home.html',{'rooms':rooms})
 
 def room(request):
-    return render(request,'room.html')
+    return render(request=request,template_name='room.html')
