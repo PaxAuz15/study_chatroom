3e76b952d005c95ad1ecd3d3939b0061d55d8c37
Luis Auz <luisauz.geek@gmail.com>
2021-10-25 18:28:34 -0500

    feat: using djago template engine to href on <a> tag and change dinamically the urls

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns:list = [
     path('', views.home, name="home"),
-    path('room/<str:pk>', views.room, name="room"),
+    path('rooms/<str:pk>', views.room, name="room"),
 ]
