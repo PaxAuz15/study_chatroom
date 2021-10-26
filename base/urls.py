ed0164b610b2b2db7f031174c718a27f051e9cbb
Luis Auz <luisauz.geek@gmail.com>
2021-10-26 18:53:53 -0500

    feat: adding url to view function

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,5 @@ urlpatterns:list = [
     path('', views.home, name="home"),
     path('rooms/<str:pk>', views.room, name="room"),
 
-    path('create-room/', views.createRoom, name="create-room")
+    path('create-room/', views.createRoom, name="create-room"),
 ]
