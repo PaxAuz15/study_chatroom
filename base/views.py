c0dfe070b3f99c02180cd355ac4310ba43a8db6e
Luis Auz <luisauz.geek@gmail.com>
2021-10-25 21:14:07 -0500

    feat: render function to room form

--- base/views.py
+++ base/views.py
@@ -11,3 +11,7 @@ def room(request,pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request=request,template_name='base/room.html',context=context)
+
+def createRoom(request):
+    context = {}
+    return render(request,'base/room_form.html',context)
