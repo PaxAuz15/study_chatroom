d6f89d4fa15f1b0fb4e61b3a3a8fc68584c0167a
Luis Auz <luisauz.geek@gmail.com>
2021-10-25 21:14:32 -0500

    feat: adding path to render function

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns:list = [
     path('', views.home, name="home"),
     path('rooms/<str:pk>', views.room, name="room"),
+
+    path('create-room/', views.createRoom, name="create-room")
 ]
