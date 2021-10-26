a86cc02d90520b02fcc916287df275127fb4d5ce
Luis Auz <luisauz.geek@gmail.com>
2021-10-25 20:21:36 -0500

    feat: Create Topic Model DB

--- base/models.py
+++ base/models.py
@@ -4,6 +4,12 @@ from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Room(models.Model):
     # host = 
     # topic = 
